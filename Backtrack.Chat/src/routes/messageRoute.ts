93f1a67db0992b3a55f04ddb25c1dd9ab7066c79
catlinh04 <catlinh8568@gmail.com>
2025-12-23 11:45:31 +0700

    feat: Update Docker configuration and improve dependency management

--- Backtrack.Chat/src/routes/messageRoute.ts
+++ Backtrack.Chat/src/routes/messageRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import MessageController from '../controllers/messageController';
+import MessageController from '../controllers/MessageController';
 
 /**
  * Message routes using class-based controller with @AsyncHandler decorator
