82bc5a8127a486749d54ef38557fd2062471d421
Nhatthang27 <nhatthang270404@gmail.com>
2026-01-03 14:18:09 +0700

    feat: Add MongoDB configuration and service to docker-compose for backtrack-qr-api

--- Backtrack.QR/src/infrastructure/database/connect.ts
+++ Backtrack.QR/src/infrastructure/database/connect.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '@/src/shared/configs/constants.js';
 import { env } from '@/src/shared/configs/env.js';
+import { en } from 'zod/locales';
 
 export const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(env.DATABASE_URI, {
-            dbName: DB_NAME,
-        });
-        console.log(`Connected to MongoDB database successfully. ${mongoose.connection.name}`);
-    } catch (error) {
-        console.error('Error connecting to MongoDB database:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(env.DATABASE_URI, {
+      dbName: DB_NAME,
+    });
+    console.log(`Connected to MongoDB database successfully. ${env.DATABASE_URI}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB database:', error);
+    process.exit(1);
+  }
 };
