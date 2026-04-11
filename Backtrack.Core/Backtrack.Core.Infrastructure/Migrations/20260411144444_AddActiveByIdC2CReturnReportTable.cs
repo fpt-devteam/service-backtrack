34271df9d9dbf116a9e84b047fa94bd295b34068
catlinh04 <catlinh8568@gmail.com>
2026-04-11 22:08:37 +0700

    Add ActivatedById column to C2CReturnReport table
    - Introduced a new column 'activated_by_id' of type character varying(255) to the 'c2c_return_reports' table.
    - Updated the ApplicationDbContextModelSnapshot to reflect the new column.
    - Created migration file '20260411144444_AddActiveByIdC2CReturnReportTable' to handle the addition of the new column.

---
+++ Backtrack.Core/Backtrack.Core.Infrastructure/Migrations/20260411144444_AddActiveByIdC2CReturnReportTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backtrack.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActiveByIdC2CReturnReportTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "activated_by_id",
+                table: "c2c_return_reports",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "activated_by_id",
+                table: "c2c_return_reports");
+        }
+    }
+}
