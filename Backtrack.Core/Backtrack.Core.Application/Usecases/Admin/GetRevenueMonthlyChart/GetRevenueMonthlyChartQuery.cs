6eb3f6f98fa7ca39d448bdc4af3d07cb9248330b
catlinh04 <catlinh8568@gmail.com>
2026-05-03 20:06:37 +0700

    fix: Update PaymentHistoryRepository to exclude deleted records in revenue calculations

--- Backtrack.Core/Backtrack.Core.Application/Usecases/Admin/GetRevenueMonthlyChart/GetRevenueMonthlyChartQuery.cs
+++ Backtrack.Core/Backtrack.Core.Application/Usecases/Admin/GetRevenueMonthlyChart/GetRevenueMonthlyChartQuery.cs
@@ -1,9 +1,11 @@
+using System.Text.Json.Serialization;
 using MediatR;
 
 namespace Backtrack.Core.Application.Usecases.Admin.GetRevenueMonthlyChart;
 
 public sealed record GetRevenueMonthlyChartQuery : IRequest<List<MonthlyRevenueChartItem>>
 {
+    [JsonIgnore]
     public required string AdminUserId { get; init; }
     public int Months { get; init; } = 12;
 }
