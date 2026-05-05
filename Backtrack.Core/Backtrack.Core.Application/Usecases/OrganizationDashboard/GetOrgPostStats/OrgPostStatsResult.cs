7d0a327c6dd883c795b61a2aeb2b737a95bf1765
catlinh04 <catlinh8568@gmail.com>
2026-05-05 23:01:06 +0700

    refactor: Simplify OrgPostStatsResult and GetOrgPostStatsHandler to focus on FoundPosts

--- Backtrack.Core/Backtrack.Core.Application/Usecases/OrganizationDashboard/GetOrgPostStats/OrgPostStatsResult.cs
+++ Backtrack.Core/Backtrack.Core.Application/Usecases/OrganizationDashboard/GetOrgPostStats/OrgPostStatsResult.cs
@@ -2,14 +2,12 @@ namespace Backtrack.Core.Application.Usecases.OrganizationDashboard.GetOrgPostSt
 
 public sealed record OrgPostStatsResult
 {
-    public required int LostPosts  { get; init; }
     public required int FoundPosts { get; init; }
-    public required int Total      { get; init; }
+
     public required ThisMonthStats ThisMonth { get; init; }
 }
 
 public sealed record ThisMonthStats
 {
-    public required int Lost  { get; init; }
     public required int Found { get; init; }
 }
