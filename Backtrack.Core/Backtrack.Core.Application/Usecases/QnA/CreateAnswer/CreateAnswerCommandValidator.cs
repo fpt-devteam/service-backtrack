77517de13aa3195615ad8b7cdc5db74bddfd8d6c
catlinh04 <catlinh8568@gmail.com>
2026-06-01 20:01:48 +0700

    feat: add conditional validation for AnswerText and ImageUrls based on answer type

--- Backtrack.Core/Backtrack.Core.Application/Usecases/QnA/CreateAnswer/CreateAnswerCommandValidator.cs
+++ Backtrack.Core/Backtrack.Core.Application/Usecases/QnA/CreateAnswer/CreateAnswerCommandValidator.cs
@@ -8,6 +8,11 @@ public sealed class CreateAnswerCommandValidator : AbstractValidator<CreateAnswe
     {
         RuleFor(x => x.AnswerText)
             .NotEmpty().WithMessage("AnswerText is required.")
-            .MaximumLength(2000).WithMessage("AnswerText must not exceed 2000 characters.");
+            .MaximumLength(2000).WithMessage("AnswerText must not exceed 2000 characters.")
+            .When(x => x.Type == Domain.Constants.AnswerType.Text);
+
+        RuleFor(x => x.ImageUrls)
+            .NotEmpty().WithMessage("ImageUrls is required.")
+            .When(x => x.Type == Domain.Constants.AnswerType.Image);
     }
 }
